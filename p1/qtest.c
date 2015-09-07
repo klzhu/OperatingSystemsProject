3c40798bbb4814591c89c92ffdb8aef9f2e30968
Jane95 <janestets@bigmir.net>
2015-09-06 22:38:41 -0400

    all tests, will make it look prettier tomorrow

--- p1/qtest.c
+++ p1/qtest.c
@@ -31,7 +31,99 @@ int queue_free (queue_t*);
 int queue_length(const queue_t* queue);
 
 int queue_delete(queue_t* queue, void* item);*/
-int other=10;
+	int other=10;
+void* d=&other;
+void** dptr=&d;
+ 		/*empty q, q with 1 element*, deleting element that doesnt exist, delete first item, delete last item*/
+/*
+empty:
+prepend 
+dequeue
+append
+dequeue
+dequeue
+iterate
+DELETE
+free*/
+int b=3;
+void* n3=&b;
+queue_prepend(q,n3);
+assert(queue_length(q)==1);
+printf("%d \n", queue_dequeue(q,dptr));
+int j=*((int*) d);
+assert(j==3);
+
+int a=2;
+void* n2=&a;
+queue_prepend(q,n2);
+assert(queue_length(q)==1);
+printf("%d \n", queue_dequeue(q,dptr));
+j=*((int*) d);
+assert(j==2);
+assert(queue_dequeue(q,dptr)==-1);
+assert(queue_iterate(q, func, n3)==-1);
+assert(queue_delete(q,n3)==-1);
+free(q);
+q=queue_new();
+queue_append(q,n3);//3
+/*
+then new queue then
+with 1 element:
+append
+dequeue
+prepend
+dequeue
+iterate
+delete
+free*/
+queue_append(q,n2);//3,2
+printf("%d \n", queue_dequeue(q,dptr));
+j=*((int*) d);
+assert(j==3);
+queue_prepend(q,n3);//3,2
+printf("%d \n", queue_dequeue(q,dptr));
+j=*((int*) d);
+assert(j==3);
+printf("iteration %d \n",queue_iterate(q, func, n2));//shud print 2
+queue_delete(q,n2);
+assert(queue_length(q)==0);
+assert(queue_dequeue(q,dptr)==-1);//ERRRRR
+assert(queue_length(q)==0);
+queue_prepend(q,n3);
+free(q);
+//HOW TO TEST AFTER FREE?
+q=queue_new();
+queue_append(q,n3);
+/*
+then again new queue
+append
+prepend
+append
+delete item not existing
+prepend
+delete first item
+delete last item
+*/
+queue_prepend(q,n2);//2,3
+int e=4;
+void* n4=&e;
+queue_append(q,n4);//2,3,4
+int nonexist=0;
+void* pnonexist=&nonexist;
+assert(queue_delete(q,pnonexist)==-1);
+queue_prepend(q,pnonexist);//0,2,3,4
+queue_delete(q,pnonexist);
+queue_delete(q,n4);
+printf("iteration %d \n",queue_iterate(q, func, n2));//should print 2,3
+
+
+
+
+ 	return 1;
+ }
+
+ void freeTest(queue_t* q){
+ 	int other=10;
 void* d=&other;
 void** dptr=&d;
 
@@ -59,11 +151,17 @@ printf("%d \n", queue_dequeue(q,dptr));//0
 printf("%d \n", j);//1
 assert(j==2);
 
-assert(queue_length(q)==2);
+assert(queue_length(q)==0);
+
+
+queue_prepend(q,n2);
+
+
+queue_prepend(q,n4);
 
 int e=5;
 void* n5=&e;
-queue_append(q,n5);//4,2,5
+queue_append(q,n5);//4,2,5//append
 assert(queue_length(q)==3);
 
 int g=6;
@@ -73,11 +171,62 @@ assert(queue_length(q)==4);
 
 printf("iteration %d \n",queue_iterate(q, func, n6));
 //test iterate, and free
-
-
-
- 	return 1;
+free(q);
+printf("iteration %d \n",queue_iterate(q, func, n6));
  }
+
+
+
+void iterete(queue_t* q){
+	int other=10;
+void* d=&other;
+void** dptr=&d;
+
+int a=2;
+void* n2=&a;
+queue_prepend(q,n2);
+
+int b=3;
+void* n3=&b;
+queue_prepend(q,n3);
+
+int c=4;
+void* n4=&c;
+queue_prepend(q,n4);
+
+queue_delete(q,n3);//now 4,2
+
+printf("%d \n", queue_dequeue(q,dptr));//0
+ int j=*((int*) d);
+printf("%d \n", j);//1
+assert(j==4);
+
+printf("%d \n", queue_dequeue(q,dptr));//0
+ j=*((int*) d);
+printf("%d \n", j);//1
+assert(j==2);
+
+assert(queue_length(q)==0);
+
+
+queue_prepend(q,n2);
+
+
+queue_prepend(q,n4);
+
+int e=5;
+void* n5=&e;
+queue_append(q,n5);//4,2,5//append
+assert(queue_length(q)==3);
+
+int g=6;
+void* n6=&g;
+queue_prepend(q,n6);//6,4,2,5
+assert(queue_length(q)==4);
+
+printf("iteration %d \n",queue_iterate(q, func, n6));
+}
+
 
  void appendANDprependANDdequeue_test(queue_t* q){
 
