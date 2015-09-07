481557b7dd231fb433ff925f92f4617ccb45f7b0
Jane95 <janestets@bigmir.net>
2015-09-07 15:58:52 -0400

    organized qtest better, got rid of many prints

--- p1/qtest.c
+++ p1/qtest.c
@@ -12,25 +12,189 @@
  	printf("element: %d \n", *((int*)x));
  }
 
- int main(){
- 	queue_t* q=queue_new();
- 	printf("%d \n", queue_length(q));//0
- 	/*methods to test: int queue_prepend(queue_t*, void*);
 
-int queue_append(queue_t*, void*);
+void freeTest(queue_t* q){
+ 	int other=10;
+void* d=&other;
+void** dptr=&d;
+
+int a=2;
+void* n2=&a;
+queue_prepend(q,n2);
+
+int b=3;
+void* n3=&b;
+queue_prepend(q,n3);
+
+int c=4;
+void* n4=&c;
+queue_prepend(q,n4);
+
+queue_delete(q,n3);//now 4,2
+
+assert( queue_dequeue(q,dptr)==0);//0
+ int j=*((int*) d);
+assert(j==4);
+
+assert( queue_dequeue(q,dptr)==0);//0
+ j=*((int*) d);
+assert(j==2);
+
+assert(queue_length(q)==0);
 
 
-int queue_dequeue(queue_t*, void**);
-
-typedef void (*func_t)(void*, void*);
-int queue_iterate(queue_t*, func_t, void*);
+queue_prepend(q,n2);
 
 
-int queue_free (queue_t*);
+queue_prepend(q,n4);
 
-int queue_length(const queue_t* queue);
+int e=5;
+void* n5=&e;
+queue_append(q,n5);//4,2,5//append
+assert(queue_length(q)==3);
 
-int queue_delete(queue_t* queue, void* item);*/
+int g=6;
+void* n6=&g;
+queue_prepend(q,n6);//6,4,2,5
+assert(queue_length(q)==4);
+
+printf("iteration %d \n",queue_iterate(q, func, n6));//shud b 6,4,2,5
+//test iterate, and free
+free(q);
+printf("iteration %d \n",queue_iterate(q, func, n6));//shud b 0 elements
+ }
+
+
+
+void iterateTest(queue_t* q){
+	int other=10;
+void* d=&other;
+void** dptr=&d;
+
+int a=2;
+void* n2=&a;
+queue_prepend(q,n2);
+
+int b=3;
+void* n3=&b;
+queue_prepend(q,n3);
+
+int c=4;
+void* n4=&c;
+queue_prepend(q,n4);
+
+queue_delete(q,n3);//now 4,2
+
+assert( queue_dequeue(q,dptr)==0);//0
+ int j=*((int*) d);
+assert(j==4);
+
+assert(queue_dequeue(q,dptr)==0);//0
+ j=*((int*) d);
+assert(j==2);
+
+assert(queue_length(q)==0);
+
+
+queue_prepend(q,n2);
+
+
+queue_prepend(q,n4);
+
+int e=5;
+void* n5=&e;
+queue_append(q,n5);//4,2,5//append
+assert(queue_length(q)==3);
+
+int g=6;
+void* n6=&g;
+queue_prepend(q,n6);//6,4,2,5
+assert(queue_length(q)==4);
+
+printf("iteration %d \n",queue_iterate(q, func, n6));//shud print elements 6,4,2,5
+}
+
+
+ void appendANDprependANDdequeue_test(queue_t* q){
+
+int other=10;
+void* d=&other;
+void** dptr=&d;
+
+
+ 	int a=2;
+void* n2=&a;
+queue_append(q,n2);
+
+int b=3;
+void* n3=&b;
+queue_append(q,n3);
+int c=4;
+void* n4=&c;
+queue_prepend(q,n4);
+//q shud b 4,2,3
+assert( queue_dequeue(q,dptr)==0);
+int j=*((int*) d);
+assert(j==4);
+
+assert( queue_dequeue(q,dptr)==0);
+ j=*((int*) d);
+assert(j==2);
+
+assert( queue_dequeue(q,dptr)==0);
+ j=*((int*) d);
+assert(j==3);
+
+ }
+
+
+
+
+ void prependANDdequeueANDdelete_test(queue_t* q){
+ 	int i=1;
+void* n=&i;
+int other=10;/*
+void* d=&other;*/
+queue_prepend(q,n);
+void* d=&other;
+void** dptr=&d;
+assert( queue_length(q)==1);//1
+assert( queue_dequeue(q,dptr)==0);//0
+int j=*((int*) d);
+assert(j==1);
+//printf("%d \n", queue_dequeue(q,dptr));// will return -1 in future
+int a=2;
+void* n2=&a;
+queue_prepend(q,n2);
+
+int b=3;
+void* n3=&b;
+queue_prepend(q,n3);
+
+int c=4;
+void* n4=&c;
+queue_prepend(q,n4);
+
+queue_delete(q,n3);//now 4,2
+
+assert( queue_dequeue(q,dptr)==0);
+ j=*((int*) d);
+assert(j==4);
+
+assert( queue_dequeue(q,dptr)==0);
+ j=*((int*) d);
+assert(j==2);
+
+
+ 	}
+
+
+
+ 	void cornercasesForEach(queue_t* q){
+ 		/*int other=10;
+void* d=&other;
+void** dptr=&d;*/
+ 		/*empty q, q with 1 element*, deleting element that doesnt exist, delete first item, delete last item*/
 	int other=10;
 void* d=&other;
 void** dptr=&d;
@@ -49,7 +213,7 @@ int b=3;
 void* n3=&b;
 queue_prepend(q,n3);
 assert(queue_length(q)==1);
-printf("%d \n", queue_dequeue(q,dptr));
+assert( queue_dequeue(q,dptr)==0);
 int j=*((int*) d);
 assert(j==3);
 
@@ -57,7 +221,7 @@ int a=2;
 void* n2=&a;
 queue_prepend(q,n2);
 assert(queue_length(q)==1);
-printf("%d \n", queue_dequeue(q,dptr));
+assert( queue_dequeue(q,dptr)==0);
 j=*((int*) d);
 assert(j==2);
 assert(queue_dequeue(q,dptr)==-1);
@@ -77,14 +241,14 @@ iterate
 delete
 free*/
 queue_append(q,n2);//3,2
-printf("%d \n", queue_dequeue(q,dptr));
+assert( queue_dequeue(q,dptr)==0);
 j=*((int*) d);
 assert(j==3);
 queue_prepend(q,n3);//3,2
-printf("%d \n", queue_dequeue(q,dptr));
+assert( queue_dequeue(q,dptr)==0);
 j=*((int*) d);
 assert(j==3);
-printf("iteration %d \n",queue_iterate(q, func, n2));//shud print 2
+printf("iteration %d \n",queue_iterate(q, func, n2));//shud print that element is 2
 queue_delete(q,n2);
 assert(queue_length(q)==0);
 assert(queue_dequeue(q,dptr)==-1);
@@ -115,7 +279,31 @@ queue_prepend(q,pnonexist);//0,2,3,4
 queue_delete(q,pnonexist);
 queue_delete(q,n4);
 assert(queue_length(q)==2);
-printf("iteration %d \n",queue_iterate(q, func, n2));//should print 2,3
+printf("iteration %d \n",queue_iterate(q, func, n2));//should print elements 2,3
+
+ 	}
+
+
+
+
+
+
+ int main(){
+ 	queue_t* q=queue_new();
+ 	assert( queue_length(q)==0);//0
+freeTest(q);
+//free(q);
+q=queue_new();
+iterateTest(q);
+free(q);
+q=queue_new();
+appendANDprependANDdequeue_test(q);
+free(q);
+q=queue_new();
+prependANDdequeueANDdelete_test(q);
+free(q);
+q=queue_new();
+cornercasesForEach(q);
 
 
 
@@ -123,197 +311,4 @@ printf("iteration %d \n",queue_iterate(q, func, n2));//should print 2,3
  	return 1;
  }
 
- void freeTest(queue_t* q){
- 	int other=10;
-void* d=&other;
-void** dptr=&d;
-
-int a=2;
-void* n2=&a;
-queue_prepend(q,n2);
-
-int b=3;
-void* n3=&b;
-queue_prepend(q,n3);
-
-int c=4;
-void* n4=&c;
-queue_prepend(q,n4);
-
-queue_delete(q,n3);//now 4,2
-
-printf("%d \n", queue_dequeue(q,dptr));//0
- int j=*((int*) d);
-printf("%d \n", j);//1
-assert(j==4);
-
-printf("%d \n", queue_dequeue(q,dptr));//0
- j=*((int*) d);
-printf("%d \n", j);//1
-assert(j==2);
-
-assert(queue_length(q)==0);
-
-
-queue_prepend(q,n2);
-
-
-queue_prepend(q,n4);
-
-int e=5;
-void* n5=&e;
-queue_append(q,n5);//4,2,5//append
-assert(queue_length(q)==3);
-
-int g=6;
-void* n6=&g;
-queue_prepend(q,n6);//6,4,2,5
-assert(queue_length(q)==4);
-
-printf("iteration %d \n",queue_iterate(q, func, n6));
-//test iterate, and free
-free(q);
-printf("iteration %d \n",queue_iterate(q, func, n6));
- }
-
-
-
-void iterete(queue_t* q){
-	int other=10;
-void* d=&other;
-void** dptr=&d;
-
-int a=2;
-void* n2=&a;
-queue_prepend(q,n2);
-
-int b=3;
-void* n3=&b;
-queue_prepend(q,n3);
-
-int c=4;
-void* n4=&c;
-queue_prepend(q,n4);
-
-queue_delete(q,n3);//now 4,2
-
-printf("%d \n", queue_dequeue(q,dptr));//0
- int j=*((int*) d);
-printf("%d \n", j);//1
-assert(j==4);
-
-printf("%d \n", queue_dequeue(q,dptr));//0
- j=*((int*) d);
-printf("%d \n", j);//1
-assert(j==2);
-
-assert(queue_length(q)==0);
-
-
-queue_prepend(q,n2);
-
-
-queue_prepend(q,n4);
-
-int e=5;
-void* n5=&e;
-queue_append(q,n5);//4,2,5//append
-assert(queue_length(q)==3);
-
-int g=6;
-void* n6=&g;
-queue_prepend(q,n6);//6,4,2,5
-assert(queue_length(q)==4);
-
-printf("iteration %d \n",queue_iterate(q, func, n6));
-}
-
-
- void appendANDprependANDdequeue_test(queue_t* q){
-
-int other=10;
-void* d=&other;
-void** dptr=&d;
-
-
- 	int a=2;
-void* n2=&a;
-queue_append(q,n2);
-
-int b=3;
-void* n3=&b;
-queue_append(q,n3);
-int c=4;
-void* n4=&c;
-queue_prepend(q,n4);
-//q shud b 4,2,3
-printf("%d \n", queue_dequeue(q,dptr));
-int j=*((int*) d);
-printf("%d \n", j);
-assert(j==4);
-
-printf("%d \n", queue_dequeue(q,dptr));
- j=*((int*) d);
-printf("%d \n", j);
-assert(j==2);
-
-printf("%d \n", queue_dequeue(q,dptr));
- j=*((int*) d);
-printf("%d \n", j);
-assert(j==3);
-
- }
-
-
-
-
- void prependANDdequeueANDdelete_test(queue_t* q){
- 	int i=1;
-void* n=&i;
-int other=10;/*
-void* d=&other;*/
-queue_prepend(q,n);
-void* d=&other;
-void** dptr=&d;
-printf("%d \n", queue_length(q));//1
-printf("%d \n", queue_dequeue(q,dptr));//0
-int j=*((int*) d);
-printf("%d \n", j);//1
-assert(j==1);
-//printf("%d \n", queue_dequeue(q,dptr));// will return -1 in future
-int a=2;
-void* n2=&a;
-queue_prepend(q,n2);
-
-int b=3;
-void* n3=&b;
-queue_prepend(q,n3);
-
-int c=4;
-void* n4=&c;
-queue_prepend(q,n4);
-
-queue_delete(q,n3);//now 4,2
-
-printf("%d \n", queue_dequeue(q,dptr));//0
- j=*((int*) d);
-printf("%d \n", j);//1
-assert(j==4);
-
-printf("%d \n", queue_dequeue(q,dptr));//0
- j=*((int*) d);
-printf("%d \n", j);//1
-assert(j==2);
-
-
- 	}
-
-
-
- 	void cornercasesForEach(queue_t* q){
- 		/*int other=10;
-void* d=&other;
-void** dptr=&d;*/
- 		/*empty q, q with 1 element*, deleting element that doesnt exist, delete first item, delete last item*/
-
- 	}
+ 
