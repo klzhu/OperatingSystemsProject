3120895a3d908367383a4a68da60df7dac0f27b7
Kevin Zhu <klzhu1@gmail.com>
2015-09-12 23:34:40 -0400

    Semaphore implementation

--- p1/synch.c
+++ p1/synch.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include "defs.h"
 #include "synch.h"
@@ -16,25 +17,72 @@
  * Semaphores.
  */
 struct semaphore {
-    /* This is temporary so that the compiler does not error on an empty struct.
-     * You should replace this with your own struct members.
-     */
-    int tmp;
+	int lock;
+	int count;
+	queue_t* semaWaitQ;
 };
 
 
 semaphore_t* semaphore_create() {
-    return NULL;
+	semaphore_t *s = malloc(sizeof(semaphore_t));
+	if (s == NULL) return NULL;
+
+	s->count = 0;
+	s->lock = 0; //signifies lock is released
+	s->semaWaitQ = queue_new();
+
+	if (s->semaWaitQ == NULL) return NULL;
+
+	return s;
 }
 
 void semaphore_destroy(semaphore_t *sem) {
+	assert(sem != NULL);
+	assert(sem->semaWaitQ != NULL);
+	if (sem == NULL) return;
+
+	int freeQueueSuccess = queue_free(sem->semaWaitQ);
+	if (freeQueueSuccess == -1) return;
+
+	free(sem);
 }
 
 void semaphore_initialize(semaphore_t *sem, int cnt) {
+	assert(sem != NULL);
+	assert(cnt != NULL);
+	if (sem == NULL || cnt == NULL) return;
+
+	sem->count = cnt;
 }
 
 void semaphore_P(semaphore_t *sem) {
+	assert(sem != NULL);
+	assert(sem->semaWaitQ != NULL);
+	if (sem == NULL || sem->semaWaitQ == NULL) return;
+
+	if (sem->count > 0) sem->count--;
+	else
+	{
+		queue_append(sem->semaWaitQ, minithread_self);
+		minithread_stop(); //should this be a thread yield or a thread stop?
+	}
 }
 
 void semaphore_V(semaphore_t *sem) {
+	assert(sem != NULL);
+	assert(sem->semaWaitQ != NULL);
+	if (sem == NULL || sem->semaWaitQ == NULL) return;
+
+	if (queue_length(sem->semaWaitQ) == 0) sem->count++;
+	else
+	{
+		//in class, the professor said we should assert that sem->count == 0 here, not sure why
+		minithread_t** t = NULL;
+		int dequeueSuccess = queue_dequeue(sem->semaWaitQ, t);
+
+		assert(t != NULL);
+		if (dequeueSuccess == -1) return;
+		
+		minithread_start(*t);
+	}
 }
