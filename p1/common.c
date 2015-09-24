6c7a7db66ddf0f6d9d1905abc449c6e4c583ed13
Kevin Zhu <klzhu1@gmail.com>
2015-09-24 19:08:35 -0400

    Added files from p2

---
+++ p1/common.c
@@ -0,0 +1,9 @@
+/*
+ * Implementations for common utilities.
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include "common.h"
+
+ // Add any function implementations here
---
+++ p1/common.h
@@ -0,0 +1,9 @@
+/*
+ * Common utilities header file
+ */
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+// Add any constants, function signatures, etc. here
+
+#endif /*__COMMON_H__*/
