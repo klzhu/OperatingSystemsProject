11c9bc8db2c070387edbf3aaae56cdbd4679f3cf
Jacob Herrera <jeh365@cornell.edu>
2015-10-21 09:56:55 -0400

    created networks 8,9,10
    up to net8 in makefile

--- p1/network8.c
+++ p1/network8.c
@@ -1,10 +1,9 @@
-/* Network test program 7
-	
-	Tests the bounds of memory by creating one thread, which
-	creates 40,000 ports, sends a message over them, and then
-	destroys the created ports.
+/* Network test program 8
 
-	USAGE: ./network7 <port>
+	Tests the behavior of minimsg_recieve when data is longer than the buffer
+	Output should still be "Hello, world!\n"
+
+	USAGE: ./network8 <port>
 
 	where <port> is the minimsg port to use
 */
@@ -18,40 +17,35 @@
 #include <stdio.h>
 #include <string.h>
 
-#define BUFFER_SIZE 256
-#define MAX_COUNT 40000
+#define BUFFER_SIZE 14
 
 
 miniport_t *listen_port;
 miniport_t *send_port;
 
-char text[] = "Hello, world!\n";
-int textlen = 14;
+char text[] = "Hello, world!\nGoodbye, world!\n";
+int textlen = 30;
 
 int
 thread(int* arg) {
 	char buffer[BUFFER_SIZE];
 	int length = BUFFER_SIZE;
-	int i;
 	miniport_t *from;
 	network_address_t my_address;
+
 	network_get_my_address(my_address);
-	
-	for (i = 0; i < MAX_COUNT; i++)
-	{
-		listen_port = miniport_create_unbound(i);
-		send_port = miniport_create_bound(my_address, i);
+	listen_port = miniport_create_unbound(0);
+	send_port = miniport_create_bound(my_address, 0);
 
-		minimsg_send(listen_port, send_port, text, textlen);
-		minimsg_receive(listen_port, &from, buffer, &length);
-		printf("%s", buffer);
-		miniport_destroy(from);
+	minimsg_send(listen_port, send_port, text, textlen);
+	minimsg_receive(listen_port, &from, buffer, &length);
+	printf("%s", buffer);
 
-	}
 	return 0;
 }
 
 int
+
 main(int argc, char** argv) {
 	short fromport;
 	fromport = atoi(argv[1]);
@@ -60,3 +54,5 @@ main(int argc, char** argv) {
 	minithread_system_initialize(thread, NULL);
 	return -1;
 }
+
+
