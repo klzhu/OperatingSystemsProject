06baf27171b57095f9af68ecccf815aef93e0975
Kevin Zhu <klzhu1@gmail.com>
2015-10-21 20:29:14 -0400

    Remove AbortGracefully from common.h because we use AbortOnCondition in defs.h

--- p1/common.h
+++ p1/common.h
@@ -4,13 +4,6 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
- /* Macro to fail gracefully. If condition, fail and give error message */
-#define AbortGracefully(cond,message)                      	\
-    if (cond) {                                             \
-        printf("Abort: %s:%d, MSG:%s\n",                  	\
-               __FILE__, __LINE__, message); 				\
-        exit(1);                                             \
-	    }
 
 // Add any constants, function signatures, etc. here
 
