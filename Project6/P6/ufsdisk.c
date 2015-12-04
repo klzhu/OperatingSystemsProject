9c3cac1382553459388dd190111184027d63018c
Kevin Zhu <klzhu1@gmail.com>
2015-12-04 18:08:57 -0500

    in prog

--- Project6/P6/ufsdisk.c
+++ Project6/P6/ufsdisk.c
@@ -22,18 +22,6 @@
 #include "block_if.h"
 #include "ufsdisk.h"
 
-#define NUM_BITS_IN_BYTES				8	/* 8 bits in a byte */
-#define NUM_DIRECT_BLOCKS				REFS_PER_INODE - 3 /* # of blocks by direct pointers */
-#define NUM_SINGLE_INDIRECT_BLOCKS		NUM_DIRECT_BLOCKS + REFS_PER_BLOCK /* # of blocks by the single indirect pointer*/	
-#define NUM_DOUBLE_INDIRECT_BLOCKS		NUM_SINGLE_INDIRECT_BLOCKS + (REFS_PER_BLOCK * REFS_PER_BLOCK) /* # of blocks by the double indirect pointer */	
-#define NUM_TRIPLE_INDIRECT_BLOCKS		NUM_DOUBLE_INDIRECT_BLOCKS + (REFS_PER_BLOCK * REFS_PER_BLOCK * REFS_PER_BLOCK) /* # of blocks by the triple pointer */	
-#define SINGLE_INDIRECT_BLOCK_INDEX		REFS_PER_INODE - 3		
-#define DOUBLE_INDIRECT_BLOCK_INDEX		REFS_PER_INODE - 2		
-#define TRIPLE_INDIRECT_BLOCK_INDEX		REFS_PER_INODE - 1	
- // capacity (# of blocks) of an indirect pointer at each level
-const int NUM_BLOCKS_LEVEL[3] = { 1, REFS_PER_BLOCK, REFS_PER_BLOCK*REFS_PER_BLOCK };
-
-
  /* Temporary information about the file system and a particular inode.
  * Convenient for all operations.
  */
--- Project6/P6/ufsdisk_chk.c
+++ Project6/P6/ufsdisk_chk.c
@@ -8,186 +8,146 @@
 #include <string.h>
 #include "block_if.h"
 #include "ufsdisk.h"
+#include <math.h>
 
-static unsigned int log_rpb;		// log2(REFS_PER_BLOCK)
-
-struct block_info {
-	enum { BI_UNKNOWN, BI_SUPER, BI_INODE, BI_INDIR, BI_DATA, BI_FREELIST, BI_FREE } status;
+/* Temporary information about the file system and a particular inode.
+* Convenient for all operations.
+*/
+struct ufs_snapshot {
+	union ufs_block superblock;
+	union ufs_block inodeblock;
+	block_no inode_blockno;
+	struct ufs_inode *inode;
 };
 
-/* Stupid ANSI C compiler leaves shifting by #bits in unsigned int or more
- * undefined, but the result should clearly be 0...
- */
-static block_no log_shift_r(block_no x, unsigned int nbits){
-	if (nbits >= sizeof(block_no) * 8) {
-		return 0;
-	}
-	return x >> nbits;
+/* The state of a virtual block store, which is identified by an inode number.
+*/
+struct ufs_state {
+	block_if below;			// block store below
+	unsigned int inode_no;	// inode number in file system
+};
+
+static void ufs_setbit(struct ufs_snapshot *snapshot, block_if below, block_no blockNum, char* freebitmaparray) {
+	//figure out what index the block num is stored at
+	int index = blockNum / NUM_BITS_IN_BYTES; //which byte in our block our block is represented in
+	int bitToReset = blockNum % NUM_BITS_IN_BYTES; //which bit in our byte our block is represented as
+
+	unsigned char uChar = 0x1;
+	freebitmaparray[index] |= ~(uChar <<= abs(bitToReset - 7));
+
+	return 0;
 }
 
-static int check_inode(block_if below, block_no nblocks, block_no node, unsigned int nlevels, block_no offset, block_no fs_nblocks, struct block_info *bi){
-	/* Basic sanity checks.
-	 */
-	if (node == 0) {
-		return 1;
-	}
-	if (node >= fs_nblocks) {
-		fprintf(stderr, "!!TDERR: --> %u %u %u\n", node, fs_nblocks, offset);
-		fprintf(stderr, "!!TDCHK: block off the underlying file system\n");
-		return 0;
-	}
-	if (bi[node].status != BI_UNKNOWN) {
-		fprintf(stderr, "!!TDCHK: data block already used\n");
-		return 0;
-	}
-
-	/* If it's a data block, mark that and return.
-	 */
-	if (nlevels == 0) {
-		bi[node].status = BI_DATA;
-		return 1;
-	}
-
-	/* Scan the tree below.  Start with getting the indirect block and
-	 * calculate the size of the tree below.
-	 */
-	bi[node].status = BI_INDIR;
-	struct treedisk_indirblock ib;
-	(*below->read)(below, node, (block_t *) &ib);
-	nlevels--;
-	block_no size = 1 << (nlevels * log_rpb);
-
-	/* Now scan through the block references.
-	 */
-	unsigned int i;
-	for (i = 0; i < REFS_PER_BLOCK; i++) {
-		if (!check_inode(below, nblocks, ib.refs[i], nlevels, offset, fs_nblocks, bi)) {
-			return 0;
+static int ufsdisk_traverseufstree(struct ufs_snapshot *snapshot, block_if below, int nlevels, block_no blocknum, char* freebitmaparray)
+{
+	if (nlevels == 0) ufs_setbit(snapshot, below, blocknum, freebitmaparray);
+	else { //we have to traverse the indirect blocks to get to our data blocks
+		nlevels--;
+		struct ufs_indirblock indirblock;
+		if ((*below->read)(below, blocknum, (block_t *)&indirblock) < 0) {
+			fprintf(stderr, "!!TDERR: Failure to read data block in ufsdisk_updatefreebits\n");
+			return -1;
 		}
-		offset += size;
-		if (offset >= nblocks) {
-			break;
+		int k;
+		for (k = 0; k < REFS_PER_BLOCK; k++)
+		{
+			if (indirblock.refs[k] != 0) { //if it is not a hole
+				ufsdisk_traverseindblocks(snapshot, below, nlevels, indirblock.refs[k]);
+				indirblock.refs[k] = 0;
+			}
+		}
+		ufs_freebit(snapshot, below, blocknum);
+		if ((*below->write)(below, snapshot->inode_blockno, (block_t *)&snapshot->inodeblock) < 0) {
+			fprintf(stderr, "!!TDERR: Failed to write inode back to disk in ufsdisk_setsize\n");
+			return -1;
 		}
 	}
-
-	return 1;
+	return 0;
 }
 
-int treedisk_check(block_if below){
-	struct block_info *bi = 0;
-	block_no fs_nblocks = (*below->nblocks)(below);
-	block_no b;
+static int ufsdisk_chk_validate_freebitblocks(struct ufs_snapshot *snapshot, block_if bi, block_no ndatablocks, block_no nfreebitmapblocks)
+{
+	//create an array that stores a bit to represent each free block
+	size_t len = ceil(ndatablocks / NUM_BITS_IN_BYTES);
+	char *freebitmapblockcheck = malloc(sizeof(len));
+	memset(freebitmapblockcheck, 0, len);
 
-	if (fs_nblocks == 0) {
-		fprintf(stderr, "!!TDCHK: empty underlying storage\n");
-		return 0;
+	//if there is a partial byte, set the extra bits to 1
+	unsigned int leftoverbits = ndatablocks % NUM_BITS_IN_BYTES;
+	if (leftoverbits > 0)
+	{
+		unsigned char uChar = 0xFF;
+		uChar >>= leftoverbits;
+		freebitmapblockcheck[len-1] = (char)uChar;
 	}
 
-	/* Calculate log2(REFS_PER_BLOCK).
-	 */
-	log_rpb = 0;
-	do {
-		log_rpb++;
-	} while (((REFS_PER_BLOCK - 1) >> log_rpb) != 0);
-
-	/* Get the superblock.
-	 */
-	union treedisk_block superblock;
-	(*below->read)(below, 0, (block_t *) &superblock);
-
-	/* Check the superblock.
-	 */
-	if (1 + superblock.superblock.n_inodeblocks > fs_nblocks) {
-		fprintf(stderr, "!!TDERR: %u %u\n", superblock.superblock.n_inodeblocks, fs_nblocks);
-		fprintf(stderr, "!!TDCHK: not enough room for inode blocks\n");
-		return 0;
-	}
-	if (superblock.superblock.free_list >= fs_nblocks) {
-		fprintf(stderr, "!!TDCHK: free list ref in superblock too large\n");
-		return 0;
-	}
-
-	/* Initialie the block info.
-	 */
-	bi = (struct block_info *) calloc(fs_nblocks, sizeof(*bi));
-	bi[0].status = BI_SUPER;
-	for (b = 1; b <= superblock.superblock.n_inodeblocks; b++) {
-		bi[b].status = BI_INODE;
-	}
-
-	/* Scan the inode blocks.
-	 */
-	struct treedisk_inodeblock tib;
-	for (b = 1; b <= superblock.superblock.n_inodeblocks; b++) {
-		(*below->read)(below, b, (block_t *) &tib);
-
-		/* Scan the inodes in the block.
-		 */
-		unsigned int i;
-		for (i = 0; i < INODES_PER_BLOCK; i++) {
-			struct treedisk_inode *ti = &tib.inodes[i];
-			if (ti->nblocks != 0) {
-				unsigned int nlevels = 0;
-				while (log_shift_r(ti->nblocks - 1, nlevels * log_rpb) != 0) {
-					nlevels++;
-				}
-				if (!check_inode(below, ti->nblocks, ti->root, nlevels, 0, fs_nblocks, bi)) {
-					free(bi);
-					return 0;
-				}
+	//iterate through all inodes to update this freebitmapblockcheck array
+	union ufs_block inodeblock;
+	int inodeblockindex;
+	int nlevels = -1;
+	int index;
+	struct ufs_state *us = bi->state;
+	for (inodeblockindex = 1; inodeblockindex <= snapshot->superblock.superblock.n_inodeblocks; inodeblockindex++)
+	{
+		if ((*us->below->read)(us->below, inodeblockindex, (block_t *)&inodeblock) < 0) {
+			fprintf(stderr, "!!TDERR: Failure to read inode block in ufsdisk_chk_validate_freebitblocks\n");
+			return -1;
+		}
+		for (index = 0; index < REFS_PER_INODE; index++)
+		{
+			if (index < NUM_DIRECT_BLOCKS && snapshot->inode->refs[index] != 0) {
+				nlevels = 0;
+				int success = ufsdisk_traverseufstree(snapshot, us->below, nlevels, snapshot->inode->refs[index], freebitmapblockcheck);
+				if (success == -1) 	return -1;
+				snapshot->inode->refs[index] = 0;
+			}
+			else if (index == SINGLE_INDIRECT_BLOCK_INDEX && snapshot->inode->refs[SINGLE_INDIRECT_BLOCK_INDEX] != 0) { //block being read is pointed to by the single indirect block
+				nlevels = 1;
+				int success = ufsdisk_traverseufstree(snapshot, us->below, nlevels, snapshot->inode->refs[index], freebitmapblockcheck);
+				if (success == -1) 	return -1;
+				snapshot->inode->refs[index] = 0;
+			}
+			else if (index == DOUBLE_INDIRECT_BLOCK_INDEX && snapshot->inode->refs[DOUBLE_INDIRECT_BLOCK_INDEX] != 0) { //block being read is pointed to by the double indirect block
+				nlevels = 2;
+				int success = ufsdisk_traverseufstree(snapshot, us->below, nlevels, snapshot->inode->refs[index], freebitmapblockcheck);
+				if (success == -1) 	return -1;
+				snapshot->inode->refs[index] = 0;
+			}
+			else if (snapshot->inode->refs[NUM_TRIPLE_INDIRECT_BLOCKS] != 0) { //block being read is pointed to by the triple indirect block
+				nlevels = 3;
+				int success = ufsdisk_traverseufstree(snapshot, us->below, nlevels, snapshot->inode->refs[index], freebitmapblockcheck);
+				if (success == -1) 	return -1;
+				snapshot->inode->refs[index] = 0;
 			}
 		}
 	}
-	
-	/* Scan the free list.
-	 */
-	block_no fl = superblock.superblock.free_list;
-	while (fl != 0) {
-		if (fl >= fs_nblocks) {
-			fprintf(stderr, "!!TDCHK: free list block number too large\n");
-			free(bi);
-			return 0;
-		}
-		if (bi[fl].status != BI_UNKNOWN) {
-			fprintf(stderr, "!!TDCHK: free list block already in use\n");
-			free(bi);
-			return 0;
-		}
-		bi[fl].status = BI_FREELIST;
 
-		/* Read the next block off the free list.
-		 */
-		struct treedisk_freelistblock tfb;
-		(*below->read)(below, fl, (block_t *) &tfb);
-
-		unsigned int i;
-		for (i = 1; i < REFS_PER_BLOCK; i++) {
-			if (tfb.refs[i] != 0) {
-				if (tfb.refs[i] >= fs_nblocks) {
-					continue;
-				}
-				if (bi[tfb.refs[i]].status != BI_UNKNOWN) {
-					fprintf(stderr, "!!TDERR: --> %u %u %d\n", fl, tfb.refs[i],
-										bi[tfb.refs[i]].status);
-					fprintf(stderr, "!!TDCHK: duplicate block in free list\n");
-					free(bi);
-					return 0;
-				}
-				bi[tfb.refs[i]].status = BI_FREE;
-			}
-		}
-		fl = tfb.refs[0];
-	}
-
-	/* Check the blocks.
-	 */
-	for (b = 0; b < fs_nblocks; b++) {
-		if (bi[b].status == BI_UNKNOWN) {
-			fprintf(stderr, "!!TDLEAK: unaccounted for block %u\n", b);
-			break;
+	//once we've iterated through all our inodes and updated our free bit map array, check against the freebitmapblocks stored on disk
+	int bitmapindex = inodeblockindex + 1;
+	union ufs_block freebitmapblock;
+	unsigned int freebitmapcheckarrayindex = 0;
+	unsigned int freebitmapblockindex;
+	for (bitmapindex; bitmapindex < bitmapindex + snapshot->superblock.superblock.n_freebitmapblocks; bitmapindex++)
+	{
+		for (freebitmapcheckarrayindex = 0; freebitmapcheckarrayindex < REFS_PER_BLOCK && freebitmapcheckarrayindex < len; freebitmapcheckarrayindex++)
+		{
+			if (freebitmapblockcheck[freebitmapcheckarrayindex] != freebitmapblock.freebitmapblock.status[freebitmapblockindex]) return -1; //if there is a mismatch, return -1
+			freebitmapcheckarrayindex++;
 		}
 	}
 
-	free(bi);
-	return 1;
+	return 0;
+}
+
+//checks our file system. Returns -1 if there is a problem, 0 otherwise.
+static int ufsdisk_chk_validate_filesystem(struct ufs_snapshot *snapshot, block_if bi, block_no ndatablocks, block_no nfreebitmapblocks)
+{
+	//validate magic number
+	if (snapshot->superblock.superblock.magic_number != MAGIC_NUMBER)
+	{
+		return -1;
+	}
+
+	//check freebitmapblock bits
+	return ufsdisk_chk_validate_freebitblocks(snapshot, bi, ndatablocks, nfreebitmapblocks);
 }
